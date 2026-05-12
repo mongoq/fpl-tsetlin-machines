911a48356b98d0fc5a215caf25fd95a179a48c0e
mongoq <mongoq@das-labor.org>
2026-05-12 12:58:53 +0200

    Update runme.sh

--- xor-function/simple-xor/runme.sh
+++ xor-function/simple-xor/runme.sh
@@ -3,15 +3,9 @@
 iverilog -g2012 -Wall -o tsetlin_machine_sim tsetlin_machine_tb.sv tsetlin_machine.sv
 
 if [[ -x "./tsetlin_machine_sim" ]]; then
-
     echo "Executable 'tsetlin_machine_sim' found, executing."
-
     ./tsetlin_machine_sim
-
 else
-
     echo "Executable 'tsetlin_machine_sim' not found or not executable, aborting."
-
     exit 1
-
 fi
